15833c899a78fcc99f245a228b222e123be1f471
Pablo Schaffner <pablo@puntorigen.com>
2025-03-01 13:49:49 -0300

    - agregado contenido informe dinamico por fecha

--- src/rutas/index.py
+++ src/rutas/index.py
@@ -2,6 +2,7 @@ from nicegui_router import page, ui
 from nicegui import app
 from modelos.diario import RegistroDiario
 from pathlib import Path
+from datetime import datetime
 
 static_path = Path(__file__).parent.parent / "otros"
 print("El ****static path**** es:",static_path)
@@ -20,18 +21,23 @@ async def inicio():
         ui.button(text="Registrar Evento").props("flat").classes("text-white mt-6")
     
     try:
-        datos = await RegistroDiario.get_by_attribute(all=True, fecha="2025-02-27")
-        ui.date(value='2023-01-01', on_change=lambda e: result.set_text(e.value))
-        result = ui.label()
+        @ui.refreshable
+        async def informe_del_dia(fecha="2025-02-27"):
+            with ui.element("p"):
+                datos = await RegistroDiario.get_by_attribute(all=True, fecha=fecha)
+                if datos:
+                    ui.label("Registros encontrados:").classes("text-h5 mt-4")
+                    for reg in datos:
+                        info = f"ID: {reg.id}, Fecha: {reg.fecha}, Resumen: {reg.resumen_dia}"
+                        
+                        ui.label(info).classes("text-body1 ml-4")            
+                else:
+                    ui.label("No hay registros en la base de datos.").classes("text-body1 mt-4")
 
-        # Convert the data to a string representation
-        if datos:
-            ui.label("Registros encontrados:").classes("text-h5 mt-4")
-            for reg in datos:
-                info = f"ID: {reg.id}, Fecha: {reg.fecha}, Resumen: {reg.resumen_dia}"
-                
-                ui.label(info).classes("text-body1 ml-4")
-        else:
-            ui.label("No hay registros en la base de datos.").classes("text-body1 mt-4")
+        fecha_hoy = datetime.today().date()
+        with ui.row():
+            ui.date(value=fecha_hoy, on_change=lambda e: informe_del_dia.refresh(e.value))        
+            informe_del_dia(fecha_hoy)
+
     except Exception as e:
         ui.label(f"Error al obtener datos: {str(e)}").classes("text-negative")
